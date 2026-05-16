abc292f16a26ffa77fcb4d0df45591483a07e717
chidubemakaogu-cpu <chidubemakaogu@gmail.com>
2026-05-16 11:32:32 +0100

    chidubem

---
+++ ANC/src/App.jsx
@@ -0,0 +1,13 @@
+// import Nav from './components/Nav.jsx';
+import Events from './components/Events.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Events />
+    </div>
+  );
+}
+
+export default App;
---
+++ ANC/src/components/Events.jsx
@@ -0,0 +1,7 @@
+
+import './Events.css';
+
+function Events() {
+  return(<div><h1>love</h1></div>)
+
+export default Events;
